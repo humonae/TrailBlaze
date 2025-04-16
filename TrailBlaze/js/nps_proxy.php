6a8f79755ab217a6fed3a737edfeb733b11fb799
SamuelClark861 <123419404+SamuelClark861@users.noreply.github.com>
2025-04-15 20:57:34 -0400

    Create nps_proxy.php
    Added a proxy file to act as middle man between api so api is not exposed

---
+++ TrailBlaze/js/nps_proxy.php
@@ -0,0 +1,35 @@
+#!/usr/local/bin/php
+
+<?php
+
+// Allow cross-origin requests
+header("Access-Control-Allow-Origin: https://www.cise.ufl.edu");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Secure API key
+$apiKey = 'A4SyIUy4hdNawnXB83vtkQS4IBB2vo9CZYd3SgVW';
+
+// Get the park code from the request
+if (!isset($_GET['parkCode'])) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Missing parkCode parameter']);
+    exit();
+}
+
+$parkCode = htmlspecialchars($_GET['parkCode']);
+
+// Make the API request
+$apiUrl = "https://developer.nps.gov/api/v1/parks?parkCode={$parkCode}&api_key={$apiKey}";
+$response = file_get_contents($apiUrl);
+
+if ($response === false) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Failed to fetch data from the National Parks API']);
+    exit();
+}
+
+// Return the API response to the client
+header('Content-Type: application/json');
+echo $response;
+?>
