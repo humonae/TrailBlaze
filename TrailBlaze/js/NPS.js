d0d76bb8eee7fe8dc3daba64b12ea5b653acf0fa
SamuelClark861 <123419404+SamuelClark861@users.noreply.github.com>
2025-04-15 20:58:25 -0400

    Update NPS.js
    Updated JS to work with proxy file

--- TrailBlaze/js/NPS.js
+++ TrailBlaze/js/NPS.js
@@ -1,23 +1,16 @@
-const apiKey = 'KEY_HERE'; // Use php wrapper to hide key
-const apiUrl = 'https://developer.nps.gov/api/v1/';
+const apiProxyUrl = 'https://cise.ufl.edu/~clark.samuel/TrailBlaze/js/nps_proxy.php'; // Use HTTPS
 
-async function fetchParks() { //EVER, BISC, DRTO, CANA, DESO, FOCA, TIMU, BICY, GUIS, FOMA....
-    //note: FOCA does not return anything,  it's not a national park and is a park under TIMU
-    const parkCodes = ["EVER", "BISC", "DRTO", "CANA", "DESO", "TIMU", "BICY", "GUIS", "FOMA"];
-    try {
-        const results = await Promise.all(parkCodes.map(fetchParkInfo));
-        const Parks = {};
-        parkCodes.forEach((code, index) => {
-            Parks[code] = results[index];
-        });
-        return Parks;
-    }  catch (error) {
-        console.error('Error fetching parks:', error);
-    }
-}
-//Obtains information for each park in JSON format
 async function fetchParkInfo(parkCode) {
-    const res = await fetch(`${apiUrl}parks?parkCode=${parkCode}&api_key=${apiKey}`);
-    const data = await res.json();
-    return data.data[0];
+    try {
+        const res = await fetch(`${apiProxyUrl}?parkCode=${parkCode}`);
+        if (!res.ok) {
+            throw new Error(`Failed to fetch park info for ${parkCode}`);
+        }
+        const data = await res.json();
+        console.log(`Data for parkCode ${parkCode}:`, data); // Debugging output
+        return data.data[0];
+    } catch (error) {
+        console.error('Error fetching park info:', error);
+        return null;
+    }
 }
